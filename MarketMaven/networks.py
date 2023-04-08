23f7ebffa926850c901d5436f9dd8ca2e7667a9c
hsiavelis21 <siavelishelen@gmail.com>
2023-04-08 16:26:02 -0700

    wip

---
+++ MarketMaven/networks.py
@@ -0,0 +1,25 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+class Network():
+    def __init__(self, name, num_nodes=1, edges=[]) -> None:
+        self.name = name 
+        self.network= nx.Graph()
+        for i in range(num_nodes):
+            self.network.add_node(i)
+    
+    def get_network(self):
+        return self.network 
+    
+    def get_name(self):
+        return self.name 
+    
+    def add_edges(self, edges):
+        for node_1, node_2 in edges:
+            self.network.add_edge(node_1, node_2)
+    
+    def visualize_network(self, path):
+        nx.drawing.nx_agraph.write_dot(self.network, path)
+
+
--- MarketMaven/views.py
+++ MarketMaven/views.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
 from . import app
+from . import networks
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    curr_network = networks.Network("Basic", 5)
+    path = "test.dot"
+    curr_network.visualize_network(path)
+
+    return render_template("index.html", network_path=path)
