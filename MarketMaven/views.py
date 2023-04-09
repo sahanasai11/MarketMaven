44660217bd4dac585102fd34884e0be41475a784
sahana <sahana4sai@gmail.com>
2023-04-08 20:03:35 -0700

    UI changes

--- MarketMaven/views.py
+++ MarketMaven/views.py
@@ -6,6 +6,7 @@ from . import networks
 @app.route("/")
 def index():
     curr_network = networks.Network("Basic", 5)
+    curr_network.add_edges([(1, 2), (2,3), (3, 4), (1,3), (4,0)])
     path = "test.dot"
     src = curr_network.visualize_network(path)
 
