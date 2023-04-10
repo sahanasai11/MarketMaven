56735c1eb91c5ca5e470396ebabdcd1a14a54b91
sahana <sahana4sai@gmail.com>
2023-04-10 14:30:30 -0700

    added js helper files

---
+++ MarketMaven/static/index.js
@@ -0,0 +1,17 @@
+(function() {
+    "use strict";
+    
+    const ROOTPATH = "http://localhost:8000";
+    const DEBUG = false;
+
+    /**
+     * Initialize the homepage of the website
+     */
+    async function init() {
+
+
+    }
+
+    init();
+    
+})();
