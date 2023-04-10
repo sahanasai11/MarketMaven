84d4fb5f7c6684bdcf82eba886848b2b520e6792
hsiavelis21 <siavelishelen@gmail.com>
2023-04-10 14:29:11 -0700

    fixed data loader

--- MarketMaven/__init__.py
+++ MarketMaven/__init__.py
@@ -3,3 +3,9 @@ from flask_bootstrap import Bootstrap
 
 app = flask.Flask(__name__)
 bootstrap = Bootstrap(app)
+
+### This is to intialize loading data when the app is intialized. The data is 
+### ONLY updated if the app is restarted, not the webpage refreshed
+with app.app_context():
+    from MarketMaven.views import set_data
+    set_data()
--- MarketMaven/data_loader.py
+++ MarketMaven/data_loader.py
@@ -4,7 +4,9 @@ import pandas as pd
 class DataLoader():
     def __init__(self, path) -> None:
         self.path = path 
+        print("Loading Data")
         self.data = pd.read_csv(path)
+        print("Data Loaded")
         self.stock_dict = self.create_stock_dict()
     
     def create_stock_dict(self):
@@ -12,8 +14,9 @@ class DataLoader():
         stock_dict = {}
         for stock in stocks:
             stock_dict[stock] = []
-        
+        print("Creating Stock Dictionary")
         for i, row in self.data.iterrows():
             if '2018' in row['date']:
                 stock_dict[row['Name']].append(dict(row))
+        print("Done Creating Stock Dictionary")
         return stock_dict
