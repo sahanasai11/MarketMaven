33eca06cbcb36f7ce4020b9f48a201ee7adf2e58
sahana <sahana4sai@gmail.com>
2023-04-30 15:43:17 -0700

    update schema

--- MarketMaven/schemas.py
+++ MarketMaven/schemas.py
@@ -1,11 +1,20 @@
 from . import db
 
 # Schemas
-class MonthlyTransaction(db.Model):
 
+# Ticker symbols to permno mappings
+class Ticker(db.Model):
+    permno = db.Column(db.Integer, primary_key=True)
+    ticker = db.Column(db.String, index=True)
+
+    def __repr__(self):
+        return '<Ticker = {}, permno = {}'.format(self.ticker, self.permno)
+    
+
+class MonthlyTransaction(db.Model):
     monthly_transaction_id = db.Column(db.Integer, primary_key=True)
     # unique identifier for a traded asset (unique to a company)
-    permno = db.Column(db.Integer, index=True)
+    permno = db.Column(db.Integer, db.ForeignKey('ticker.permno'), index=True)
     # date that the asset is traded;
     date = db.Column(db.Date, index=True)
     # type of security (we only have 10 (ordinary shares) 11 (common shares))
@@ -37,5 +46,20 @@ class MonthlyTransactionFFM(db.Model):
     risk_free = db.Column(db.Float, index=True)
     market_rate = db.Column(db.Float, index=True)
 
+    def __repr__(self):
+        return '<Monthly_Transaction_FFM ID = {}, date = {}>'.format(self.monthly_transaction_ffm_id, self.date)
+    
+    
+# TODO: RUN FLASK DB MIGRATE AND FLASK DB UPGRADE SINCE THIS TABLE
+# DOESNT EXIST IN SCRIPTS YET. WANT TO CHECK W HELEN FIRST
+class MonthlyTransactionFFM(db.Model):
+    monthly_transaction_ffm_id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, index=True)
+    market_minus_risk_free = db.Column(db.Float, index=True)
+    small_minus_big = db.Column(db.Float, index=True)
+    high_minus_low = db.Column(db.Float, index=True)
+    risk_free = db.Column(db.Float, index=True)
+    market_rate = db.Column(db.Float, index=True)
 
-
+    def __repr__(self):
+        return '<Monthly_Transaction_FFM ID = {}, date = {}>'.format(self.monthly_transaction_ffm_id, self.date)
