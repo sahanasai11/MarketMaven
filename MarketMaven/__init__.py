77297b2a092bc6042885e97a1429e0bfdb07f720
sahana <sahana4sai@gmail.com>
2023-06-21 14:09:14 -0700

    change init file

--- MarketMaven/__init__.py
+++ MarketMaven/__init__.py
@@ -1,11 +1,8 @@
 import flask, csv
 from flask_bootstrap import Bootstrap
-from .config import Config
 
 app = flask.Flask(__name__)
 bootstrap = Bootstrap(app)
-app.config.from_object(Config)
-
 
 
 
